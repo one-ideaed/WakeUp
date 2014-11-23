355dd1680f861001683c9be6a7206c6234d2dc75
IAN <ioan.artamonov@gmail.com>
2014-11-24 01:57:08 +0200

    Initial check-in of module Cashflow_Monitor

---
+++ MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Cashflow_Monitor
+{
+	[Activity (Label = "Cashflow_Monitor", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 0;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			Console.WriteLine ("Step 1");
+			base.OnCreate (bundle);
+			Console.WriteLine ("Step 2");
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+			Console.WriteLine ("Step 3");
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			TextView counterText = FindViewById<TextView> (Resource.Id.countText);
+			counterText.SetText("2t", TextView.BufferType.Normal);
+			SmsReceiver smsReceiver = new SmsReceiver ();
+			Console.WriteLine ("Step 4");
+
+		}
+	}
+}
+
+
---
+++ SmsReceiver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Android.Content;
+using Android.Widget;
+using Android.App;
+
+
+namespace Cashflow_Monitor
+{
+	[BroadcastReceiver(Enabled = true, Label = "SMS Receiver")]
+	[IntentFilter(new[] { "android.provider.Telephony.SMS_RECEIVED" })] 
+	public class SmsReceiver : BroadcastReceiver
+	{
+		public override void OnReceive (Context context, Android.Content.Intent intent)
+		{
+			Console.Write ("Sms 1");
+			//Toast.MakeText (context, "SMS received", ToastLength.Long).Show ();
+			/*var nMgr = (NotificationManager)context.GetSystemService("android.provider.Telephony.SMS_RECEIVED");
+			var notification = new Notification (Resource.Drawable.Icon, "Cashflow Monitor received SMS");
+			var pendingIntent = PendingIntent.GetActivity (context, 0, new Intent (context, typeof(MainActivity)), 0);
+			notification.SetLatestEventInfo (context, "Cashflow Monitor Notification", "Here is another SMS to process", pendingIntent);
+			nMgr.Notify (0, notification);*/
+			// Instantiate the builder and set notification elements:
+			Notification.Builder builder = new Notification.Builder (context)
+				.SetContentTitle ("Cashflow Monitor")
+				.SetContentText ("Here is another SMS to process!")
+				.SetSmallIcon (Resource.Drawable.Icon);
+
+			// Build the notification:
+			Notification notification = builder.Build();
+
+			// Get the notification manager:
+			NotificationManager notificationManager =
+				context.GetSystemService (Context.NotificationService) as NotificationManager;
+
+			// Publish the notification:
+			const int notificationId = 0;
+			notificationManager.Notify (notificationId, notification);
+			Console.Write ("Sms 2");
+			//((StockActivity)context).GetStocks ();
+
+			//InvokeAbortBroadcast();
+		}
+	}
+}
+
